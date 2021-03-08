fac321efdcf6a8eb6e5cc83f7838c1951170e261
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-08 01:00:26 -0800

    adding material ui style

--- codecollective/src/App.js
+++ codecollective/src/App.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./compenents/pages/home";
+
+function App () {
+    return (
+        <Router>
+            <div>
+                
+            </div>
+        </Router>
+    )
+}
