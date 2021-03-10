39a32937e943cff5aa5cc252cf46764a338aa562
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-10 13:30:21 -0800

    added comments to explain my code in connection.js

--- coding-collective/db/connection.js
+++ coding-collective/db/connection.js
@@ -1,8 +1,11 @@
+// This requires the mongo client dependency
 const { MongoClient } = require("mongodb");
 
+// This sets up my mongo url to my free cluster
 const url = "mongodb+srv://new-user:<password>@cluster0.z4zua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(url);
 
+// This function connects to the cluster and catches any errors
 async function run () {
     try {
         await client.connect();
@@ -14,4 +17,5 @@ async function run () {
     }
 }
 
+// This calls the function so it starts the connection 
 run().catch(console.dir);
