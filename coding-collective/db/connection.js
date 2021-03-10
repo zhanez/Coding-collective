e866fe4438eed73343eeee967b3e9c2db0590473
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-10 13:27:53 -0800

    added mongodb url

--- coding-collective/db/connection.js
+++ coding-collective/db/connection.js
@@ -1,2 +1,5 @@
 const { MongoClient } = require("mongodb");
 
+const url = "mongodb+srv://new-user:<password>@cluster0.z4zua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(url);
+
