ca6c95e8980b1bb4fa5d706ebb88acd61613d8c8
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-10 13:53:25 -0800

    fixed location of db

--- coding-collective/server.js
+++ coding-collective/server.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
-const db = require("./models");
+const db = require("./db");
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
