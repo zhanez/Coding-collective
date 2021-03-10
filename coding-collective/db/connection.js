af3a930c19a132dab982d8b3769184a6386a4525
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-10 13:28:12 -0800

    finished biulding connection

--- coding-collective/db/connection.js
+++ coding-collective/db/connection.js
@@ -3,3 +3,15 @@ const { MongoClient } = require("mongodb");
 const url = "mongodb+srv://new-user:<password>@cluster0.z4zua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(url);
 
+async function run () {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+    } catch (err) {
+        console.log(err.stack);
+    } finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
