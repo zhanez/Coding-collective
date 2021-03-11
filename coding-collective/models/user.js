77c4a36acce9349d030edd5e3ea645315330e328
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-10 20:22:58 -0800

    working on user.js so that it works with mongodb

--- coding-collective/models/user.js
+++ coding-collective/models/user.js
@@ -1,5 +1,8 @@
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
 // Creating our User model
 module.exports = function(sequelize, DataTypes) {
   const User = sequelize.define("User", {
