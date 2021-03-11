99b7379ff44b5aa2129e04d997b5eaa18adf025a
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-10 20:38:52 -0800

    finished setting up models for users

--- coding-collective/models/user.js
+++ coding-collective/models/user.js
@@ -1,7 +1,21 @@
-// Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
-const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+// Creating ThirdPartySchema
+const ThirdPartyProviderSchema = mongoose.Schema({
+  provider_name: {
+    type: String,
+    default: null
+  },
+  provider_id: {
+    type: String,
+    default: null
+  },
+  provider_data: {
+    type: {},
+    default: null
+  }
+})
 
 // Creating our User model
 const UserSchema = new mongoose.Schema (
@@ -9,12 +23,40 @@ const UserSchema = new mongoose.Schema (
     name: {
       type: String
     },
-    {
-      email: {
-        type: String,
-        require: true,
-        unique: true
+    email: {
+      type: String,
+      require: true,
+      unique: true
+    },
+    email_is_verified: {
+      type: Boolean,
+      default: false
+    },
+    password: {
+      type: String
+    },
+    referral_code: {
+      type: String,
+      default: function() {
+        let hash = 0;
+        for (let i = 0; i< this.email.length; i++) {
+          hash = this.email.charCodeAt(i) + ((hash << 5) - hash);
+        }
+        let res =(hash & 0x00ffffff).toString(16).toUpperCase();
+        return "00000".substring(0, 6 - res.length) + res;
       }
+    },
+    referred_by: {
+      type: String,
+      defaull: null
+    },
+    third_party_auth: [ThirdPartyProviderSchema],
+    date: {
+      type: Date,
+      default: Date.now
     }
-  }
-)
+  },
+  { strict: false }
+);
+
+module.exports = User = mongoose.model("users", UserSchema);
