de80df53009ecdee1ef42bab8751b823bf0f5da0
Chetan Sekhon <chetansekhon@gmail.com>
2021-03-11 20:50:32 -0800

    Created sign up page, added react router dom, and created a route

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+// import logo from "./logo.svg";
+// import "./App.css";
+import SignUp from "./pages/signup"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    <Router>
+    <div>
+    <Route path="/" component={
+      SignUp
+    } />
       </div>
+      </Router>
     );
   }
 }
