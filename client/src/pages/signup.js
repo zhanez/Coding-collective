b50aa202fa0bc86cd5e1ea3fc7e97f2f8c6f936d
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-13 13:32:06 -0800

    setting up api routes for frontend connection

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {SignupForm} from "../components/Forms";
+import API from "../utils/API";
+
+function Users() {
+  const [users, setUsers] = useState([])
+  const []
+
+}
 
 function Signup() {
   return (
