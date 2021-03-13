5e7b9d91ce031c86d80f26381ec1ffc54a886e96
Wednesday <yensonyu@gmail.com>
2021-03-13 00:17:43 -0800

    set up homepage and signup/login pages template

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,40 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h2>Welcome to React</h2>
+//         </div>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+function App() {
+  return (
+    <BrowserRouter basename='/'>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+        </Wrapper>
       </div>
-    );
-  }
+    </BrowserRouter>
+  );
 }
 
 export default App;
