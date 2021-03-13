569149bd3b24b40c471832a8acf6ae181875adf4
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-13 12:54:11 -0800

    merged with origin/main and discarded my changes

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,31 +1,5 @@
 import React from "react";
 import {SignupForm} from "../components/Forms";
-import API from "../utils/API";
-
-// function Users() {
-//   const [users, setUsers] = useState([])
-//   const [formObject, setFormObject] = useState({})
-
-//   useEffect(() => {
-//     loadUsers()
-//   }, [])
-
-//   function handleInputChange(event) {
-//     const { name, value } = event.target;
-//     setFormObject({...formObject, [name]: value})
-//   };
-
-//   function handleFormSubmit(event) {
-//     event.preventDefault();
-//     if (formObject.email && formObject.password) {
-//       API.saveUser({
-//         email: formObject.email,
-//         password: formObject.password
-//       })
-//       .catch(err => console.log(err))
-//     }
-//   };
-// }
 
 function Signup() {
   return (
