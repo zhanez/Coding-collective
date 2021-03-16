a452e53d5b3ba7dd2aecb5200ead1f7e824ccded
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-15 18:22:09 -0700

    I'm trying to link the backend user authentication

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {SignupForm} from "../components/Forms";
+import API from "../utils/API";
+
+function Users() {
+
+  const [users, setUsers] = useState([]);
+  const [formObject, setFormObject] = useState({})
+
+  useEffect(() => {
+    loadUsers()
+  }, [])
+
+  function loadUsers() {
+    API.getBooks()
+      .then(res => 
+        setUsers(res.data)
+      )
+      .catch(err => console.log(err));
+  };
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormObject({...formObject, [name]: value})
+  };
+  
+  function handleFormSubmit(event) {
+    event.preventdefault();
+    if (formObject.email && formObject.password) {
+      API.saveUser({
+        email: formObject.email,
+        password: formObject.password
+      })
+        .then(res => loadUsers())
+        .catch(err => console.log(err))
+    }
+  }
+}
 
 function Signup() {
   return (
     <div>
       <h1>Create Account</h1>
-      <SignupForm />
+      <SignupForm 
+      disabled={!(formObject.email && formObject.password)}
+      onClick={handleFormSubmit} />
     </div>
   );
 }
