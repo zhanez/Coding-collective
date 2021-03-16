194a34eae0d14511131f5269514e8f002ae12770
elliefh <elliefu18@gmail.com>
2021-03-15 21:28:41 -0700

    changed logo on Navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
-import cclogo from "../Image/cclogo_long.gif";
+import cclogo from "../Image/cc_shortlogo.gif";
 
 function Navbar() {
   return (
     <nav className="navbar is-black" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <Link className="navbar-item" to="/">
-          <img src= {cclogo} alt="logo" width="112" height="28" />
+          <img src= {cclogo} alt="logo" width="80" height="50" />
         </Link>
 
         <Link role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
