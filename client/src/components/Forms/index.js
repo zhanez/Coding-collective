502dddf35ddd96615f0e5b0cd6a55aea92740d4d
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-15 21:35:34 -0700

    deleted code to work with application

--- client/src/components/Forms/index.js
+++ client/src/components/Forms/index.js
@@ -2,49 +2,19 @@ import React, {useRef} from "react";
 // import { pass } from "../../../../config/jwtPassportStrategy";
 
 export function SignupForm() {
-  const emailRef = useRef();
-  const passwordRef = useRef();
-
-  // Get the helper login function from the `useLogin` hook.
-  const login = useLogin();
-
-  const handleSubmit = async e => {
-      e.preventDefault();
-
-      const email = emailRef.current.value;
-      const password = passwordRef.current.value;
-
-      try {
-
-          // Register the user.
-          await api.register({ email, password });
-
-          // User has been successfully registered, now log them in with the same information.
-          await login({ email, password });
-
-          // User has been successfully registered, logged in and added to state. Perform any additional actions you need here such as redirecting to a new page.
-
-      } catch(err) {
-
-           // Handle error responses from the API. This will include
-           if( err.response && err.response.data ) console.log(err.response.data);
-           
-      }
-  }
-
   return (
-    <form onSubmit={handleSubmit} class="box">
+    <form  class="box">
       <div class="field">
         <label class="label">Email</label>
         <div class="control">
-          <input class="input" ref={emailRef} type="email" placeholder="e.g. alex@example.com" />
+          <input class="input"  type="email" placeholder="e.g. alex@example.com" />
         </div>
       </div>
 
       <div class="field">
         <label class="label">Password</label>
         <div class="control">
-          <input class="input" ref={passwordRef} type="password" placeholder="********" />
+          <input class="input" type="password" placeholder="********" />
         </div>
       </div>
 
@@ -54,45 +24,20 @@ export function SignupForm() {
 }
 
 export function LoginForm() {
-  const emailRef = useRef();
-  const passwordRef = useRef();
-
-  // Get the helper login function from the `useLogin` hook.
-  const login = useLogin();
-
-  const handleSubmit = async e => {
-      e.preventDefault();
-
-      const email = emailRef.current.value;
-      const password = passwordRef.current.value;
-
-      try {
-
-          await login({ email, password });
-
-          // User has been successfully logged in and added to state. Perform any additional actions you need here such as redirecting to a new page.
-
-      } catch(err) {
-
-           // Handle error responses from the API
-           if( err.response && err.response.data ) console.log(err.response.data);
-
-      }
-  }
     return (
       <div className="columns">
-      <form onSubmit={handleSubmit} class="box column is-three-fifths is-offset-one-fifth mt-5">
+      <form class="box column is-three-fifths is-offset-one-fifth mt-5">
         <div class="field">
           <label class="label">Email</label>
           <div class="control">
-            <input class="input" ref={emailRef} type="email" placeholder="e.g. alex@example.com" />
+            <input class="input" type="email" placeholder="e.g. alex@example.com" />
           </div>
         </div>
   
         <div class="field">
           <label class="label">Password</label>
           <div class="control">
-            <input class="input" ref={passwordRef} type="password" placeholder="********" />
+            <input class="input" type="password" placeholder="********" />
           </div>
         </div>
   
