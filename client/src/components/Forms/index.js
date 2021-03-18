32c5ecac0b3806ad8f21b49ba8b9b111fdb3c153
Chetan Sekhon <chetansekhon@gmail.com>
2021-03-17 21:50:42 -0700

    Added Name form to Signup

--- client/src/components/Forms/index.js
+++ client/src/components/Forms/index.js
@@ -33,6 +33,13 @@ export function SignupForm() {
   return (
     <form onSubmit={handleSubmit} class="box column is-three-fifths is-offset-one-fifth ">
       <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+          <input class="input" type="Name" ref={passwordRef} placeholder="********" />
+        </div>
+      </div>
+      
+      <div class="field">
         <label class="label">Email</label>
         <div class="control">
           <input
