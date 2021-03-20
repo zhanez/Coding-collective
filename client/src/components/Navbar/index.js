5039aa221af87ab70b6b6b53885a6323a63d464e
elliefh <elliefu18@gmail.com>
2021-03-20 12:17:06 -0700

    updated logo display

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
-import cclogo from "../Image/cclogo_long.gif";
+import cclogo from "../Image/cc_shortlogo.gif";
 import { useIsAuthenticated } from "../../utils/auth.js";
 
 function Navbar() {
@@ -11,7 +11,7 @@ function Navbar() {
     <nav className="navbar is-black" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <Link className="navbar-item" to="/">
-          <img src= {cclogo} alt="logo" width="112" height="28" />
+          <img src= {cclogo} alt="logo" width="80" height="28" />
         </Link>
 
         <Link role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
