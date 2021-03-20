ddbd624321138bc06421ce9610b02cf528b2d457
Zhane Zabala <zhanezabala97@gmail.com>
2021-03-20 10:44:24 -0700

    upload profile code added

---
+++ client/src/pages/Login 2.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {LoginForm} from "../components/Forms";
+// import background from "../images/keyboard.jpg";
+
+function Login() {
+  return (
+    // <div style={{backgroundImage:`url(${background})`}}>
+     <div className="animation"> 
+          <h1 className="glow">Sign in</h1>
+      <div className="container"> 
+      <LoginForm />
+      </div>
+    </div>
+  // </div>
+  );
+};
+
+export default Login;
