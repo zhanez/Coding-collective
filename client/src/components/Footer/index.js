d65e4d767aeb98c2ebf178e5b2cf0d709d2bd13a
Wednesday <yensonyu@gmail.com>
2021-03-20 22:56:49 -0700

    add footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div class="content has-text-centered">
+        <p>
+          &copy;<strong>Pod 17</strong> all rights reversed 2021
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -3,20 +3,20 @@ import React from "react";
 
 function Sidebar() {
   return (
-      <aside className="menu">
-        <ul className="menu-list">
-          <li>
-            <a>My Classmates</a>
-            <ul>
-                <li><a>Zhane Zabala</a></li>
-                <li><a>Ellie Fu</a></li>
-                <li><a>Marco Oseguera</a></li>
-                <li><a>Yuwen Yu</a></li>
-                <li><a>Chetan Sekhon</a></li>
-            </ul>
-          </li>
-        </ul>
-      </aside>
+    <aside className="menu">
+      <ul className="menu-list">
+        <li>
+          <a>My Classmates</a>
+          <ul>
+            <li><a>Zhane Zabala</a></li>
+            <li><a>Ellie Fu</a></li>
+            <li><a>Marco Oseguera</a></li>
+            <li><a>Yuwen Yu</a></li>
+            <li><a>Chetan Sekhon</a></li>
+          </ul>
+        </li>
+      </ul>
+    </aside>
   );
 }
 
