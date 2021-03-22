d52e1e63cdecd583badaec22509aa53fe1d31473
Wednesday <yensonyu@gmail.com>
2021-03-21 17:18:57 -0700

    set up and adjust routes, model and controller to handle with posts

--- models/feed.js
+++ models/feed.js
@@ -2,17 +2,26 @@ const mongoose =require("mongoose");
 const Schema =mongoose.Schema;
 
 const feedSchema = new Schema({
-    post: {
+    title: {
         type: String,
         required: true
     },
-    date: 
-    { type: Date, default: Date.now },
-    post_id: { 
-        type: String, 
-        default: null
+
+    content: {
+        type: String,
+        required: true
+    },
+
+    date: { 
+        type: Date, 
+        default: Date.now 
     }
+
+    // post_id: { 
+    //     type: String, 
+    //     default: null
+    // }
 });
 
-const Feed =mongoose.model("Profile", feedSchema);
+const Feed =mongoose.model("Feed", feedSchema);
 module.exports = Feed;
