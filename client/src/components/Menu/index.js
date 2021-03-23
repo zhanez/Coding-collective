d4a0babb62c07b9b190f309ed7c1a4c15750f2cf
Zhane Zabala <zhanezabala97@gmail.com>
2021-03-22 20:15:22 -0700

    title for the community page is entered

--- client/src/components/Menu/index.js
+++ client/src/components/Menu/index.js
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
 // function Menu() {
 //     return(
 //         <aside class="menu">
 //         <ul class="menu-list">
 //             <li><a>Newsfeed</a></li>
 //             <li><a>My Profile</a></li>
-//             <li>
-//             <a class="is-active">Classmates</a>
+//             <li><a class="is-active">Classmates</a>
 //             <ul>
 //                 <li><a>Zhane Zabala</a></li>
 //                 <li><a>Ellie Fu</a></li>
@@ -19,27 +17,5 @@
 //         </aside>
 // );
 // }
-=======
-function Menu() {
-    return(
-        <aside className="menu">
-        <ul className="menu-list">
-            <li><a>Newsfeed</a></li>
-            <li><a>My Profile</a></li>
-            <li>
-            <a className="is-active">Classmates</a>
-            <ul>
-                <li><a>Zhane Zabala</a></li>
-                <li><a>Ellie Fu</a></li>
-                <li><a>Marco Oseguera</a></li>
-                <li><a>Yuwen Yu</a></li>
-                <li><a>Chetan Sekhon</a></li>
-            </ul>
-            </li>
-        </ul>
-        </aside>
-);
-}
->>>>>>> b63a59bd36b41222256966929e06b1a0f88edb2d
 
 // export default Menu;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
     return(
-        <h1>YO</h1>
+        <h1> WELCOME TO CODING COLLECTIVE</h1>
     );
 }
 
