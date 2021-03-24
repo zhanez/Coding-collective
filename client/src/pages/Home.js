2613f6ab35b88a3866dcf9d4394931319e096d08
Zhane Zabala <zhanezabala97@gmail.com>
2021-03-24 14:14:33 -0700

    homepage is somewhat together

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function Home() {
     return(
-        <h1> WELCOME TO CODING COLLECTIVE</h1>
+        <div>
+        <h1 className="glow"> WELCOME TO CODING COLLECTIVE</h1>
+        
+        <h2> Where graduated UW bootcamp colleagues can connect</h2>
+        </div>
     );
 }
 
