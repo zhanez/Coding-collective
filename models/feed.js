afbecb463a0bbc58f8d4d37f6f16ba7d4029af93
elliefh <elliefu18@gmail.com>
2021-03-27 12:53:09 -0700

    add field for user name

--- models/feed.js
+++ models/feed.js
@@ -2,6 +2,10 @@ const mongoose =require("mongoose");
 const Schema =mongoose.Schema;
 
 const feedSchema = new Schema({
+  user: {
+    type: String,
+    required: true
+  },
   title: {
     type: String,
     required: true
