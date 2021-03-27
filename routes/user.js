e877729383eeef5c3ae0d099f7b52c58a6ab57aa
elliefh <elliefu18@gmail.com>
2021-03-27 11:31:23 -0700

    added back authenticateduser

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,11 @@ const userController = require("../controllers/usercontroller");
 const authenticateUser = require("./middleware/authenticateUser");
 
 router.route("/")
-  .get(authenticateUser, userController.findUser)
+  .get(userController.findUser)
   .post(userController.create);
 
 router.route("/all")
-  .get(authenticateUser, userController.findAll);
+  .get(userController.findAll);
 
 router.route("/classmate/:id")
   .get(userController.findById);
