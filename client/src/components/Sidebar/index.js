60a30e615ed9a247ca5caa4b62d556c9ec3ed06f
elliefh <elliefu18@gmail.com>
2021-03-27 11:30:14 -0700

    updated link to profile with user's unique id

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -3,29 +3,14 @@ import React, { useState, useEffect } from "react";
 import "./style.css";
 import API from "../../utils/API";
 
-<<<<<<< HEAD
-function Sidebar() {
-=======
 
 function Sidebar(props) {
->>>>>>> e5c65c5b8d1de072c6ba795eac2631d0dc9d25f2
   const [users, setUsers] = useState([]);
     useEffect(() => {
       loadUsers()
     }, [])
 
     function loadUsers() {
-<<<<<<< HEAD
-        API.getallUsers()
-            .then(res => {
-                setUsers(res.data)
-            }).catch(err => {
-              console.log(err)
-            });
-    };    
-
-    console.log('user state:', users)
-=======
       API.getallUsers()
         .then(res => {
           console.log(res.data)
@@ -34,34 +19,20 @@ function Sidebar(props) {
     };    
 
   console.log(users);
->>>>>>> e5c65c5b8d1de072c6ba795eac2631d0dc9d25f2
 
   return (
     <aside className="menu pl-6 pt-5">
       <p className="menu-label">
         My Colleagues:
       </p>
-<<<<<<< HEAD
-      <ul class="menu-list">
-        {users.map(user => <li><a href="/classmate">{user.firstName} {user.lastName}</a></li>)}
-=======
       <ul className="menu-list">
         {users.map(user => 
           <li key={user._id}>
-            <a>{user.firstName} {user.lastName}</a>
+            <a href={"/profile/" + user._id}>{user.firstName} {user.lastName}</a>
           </li>
         )}
->>>>>>> e5c65c5b8d1de072c6ba795eac2631d0dc9d25f2
       </ul>
 
-      {/* <p class="menu-label">
-        Find Posts by Category:
-      </p>
-      <ul class="menu-list">
-        <li><a>Coding Issues</a></li>
-        <li><a>Share Content</a></li>
-        <li><a>Other</a></li>
-      </ul> */}
       <p className="menu-label search-label">Search Posts:</p>
       <div className="field has-addons searchBar">
         <div className="control">
