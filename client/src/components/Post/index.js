51279386714fd1a2d37b4b1f7c68ffe38bc9b415
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-27 10:56:45 -0700

    added hook to Post component to display username for each post

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import API from "../../utils/API";
 import "./style.css";
 
 function Post(props) {
   const { posts } = props;
+
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    loadUser()
+  }, []);
+
+  function loadUser() {
+    API.loadUsers()
+      .then( res => {
+        console.log(res.data)
+        setUser(res.data)
+      }).catch(err => console.log(err));
+  };
+
   return (
     <div className="pr-6">
       <ul>
@@ -10,7 +26,7 @@ function Post(props) {
           <li key={post._id}>
             <article className=" mt-5 message">
               <div className="message-header" >
-                <p>{post.title}</p>
+                <p>{user.firstName} {user.lastName}</p>
                 <span class="tag is-warning is-light">{post.category}</span>
               </div>
               <div className="message-body">
