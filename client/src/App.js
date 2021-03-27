5efc144d4261d6d1d4dc32e0c7e4a90b21b280d5
elliefh <elliefu18@gmail.com>
2021-03-27 11:29:06 -0700

    updated profile path with ID

--- client/src/App.js
+++ client/src/App.js
@@ -14,28 +14,23 @@ import GuestRoute from "./components/auth/GuestRoute";
 import PrivateRoute from "./components/auth/PrivateRoute"
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
-<<<<<<< HEAD
-import ClassmateProfile from "./pages/Classmate";
-=======
->>>>>>> e5c65c5b8d1de072c6ba795eac2631d0dc9d25f2
 
 function App() {
 
-  useAuthTokenStore();
+  const isDone = useAuthTokenStore();
 
   return (
     <BrowserRouter basename='/'>
       <div>
         <Navbar />
-        <Wrapper>
+        {isDone && <Wrapper>
           <Route exact path="/" component={Home} />
           <GuestRoute exact path="/login" redirectTo="/community" component={Login} />
           <GuestRoute exact path="/signup" redirectTo="/community" component={Signup} />
           <PrivateRoute exact path="/community" redirectTo="/login" component={Community} />
-          <PrivateRoute exact path="/profile" redirectTo="/login" component={Profile} />
-          <PrivateRoute exact path="/classmate" component={ClassmateProfile} />
+          <PrivateRoute exact path="/profile/:id" redirectTo="/login" component={Profile}/>
           <Route exact path="/about" component={About} />
-        </Wrapper>
+        </Wrapper>}
         <Footer />
       </div>
     </BrowserRouter>
